ab0df2087f23468ad799d996e25bbc666c88ba06
Steve Gardiner <stevegardiner26@aol.com>
2019-01-23 21:18:53 -0500

    Assignment #1

---
+++ HW1_StevenGardiner.py
@@ -0,0 +1,38 @@
+# Steven Gardiner
+# CS 100 2018S Section 004
+# HW 01, January 23, 2018
+
+# Exercise 5b
+months = 12
+weeks = 52
+days = 365
+
+# Exercise 5c
+total_price = 7.23
+tax = 0.24
+net_price = 6.99
+
+# Exercise 5d
+squareDude = 'Spongebob'
+starDude = 'Patrick'
+smallDude = 'Plankton'
+
+# Exercise 1.1
+# In a print statement, what happens if you leave out one of the parentheses, or both?
+# You get a SyntaxError: invalid syntax
+
+# Exercise 1.2
+# If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+# If you leave one out it does not recognize when the string ends.
+# If you leave both out it is trying to search for a variable of the name of what you type within the parenthesis
+
+# Exercise 2.1
+# How many seconds are there in 42 minutes 42 seconds?
+42 * 60 + 42
+2562
+
+# Exercise 2.2
+# How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+10/1.61
+6.211180124223602
+
